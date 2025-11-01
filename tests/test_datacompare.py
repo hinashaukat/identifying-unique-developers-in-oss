d34657c0b303e5d0d798134f92b3fb72310cbd1f
Antti Moilanen <91624045+moilanenantti@users.noreply.github.com>
2025-11-01 18:25:23 +0200

    Fix path addition for src directory in tests

--- tests/test_datacompare.py
+++ tests/test_datacompare.py
@@ -9,7 +9,7 @@ import sys
 import os
 
 # Add src directory to path
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'src'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 
 from DataCompare import DataCompare
 
